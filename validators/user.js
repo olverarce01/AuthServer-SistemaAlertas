d086bac318f7da7c4a9543caf9b252fc19f8f869
OlverArce01 <olver.arce.limache@alumnos.uta.cl>
2023-05-02 11:42:43 -0500

    update validator messages

--- validators/user.js
+++ validators/user.js
@@ -1,12 +1,11 @@
 import {check, body} from 'express-validator';
 import {validateResult} from '../helpers/validateHelper.js';
 const validateCreate = [
-  body('username', "Ingrese un email válido")
-  .isEmail()
-  .exists(),
-  body('password', "Ingrese una contraseña válida")
-  .exists()
-  .notEmpty()
+  body('username')
+  .notEmpty().withMessage("Is Empty")
+  .isEmail().withMessage("Isn't a Email"),
+  body('password')
+  .notEmpty().withMessage("Is Empty")
   ,(req,res,next)=>{
     validateResult(req,res,next)
   }
